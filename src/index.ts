ea0668e5be511ad5f2efc8d5a3f996893bd44a47
titouancreach <titouan.creach@gmail.com>
2019-04-05 22:31:13 +0200

    graphql hello World

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("hello World");
+import express from "express";
+import expressGraphql from "express-graphql";
+import { buildSchema } from "graphql";
+
+const schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
+
+const root = {
+  message: () => "Hello World"
+};
+
+const app = express();
+
+app.use(
+  "/graphql",
+  expressGraphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+
+app.listen(process.env.PORT, () =>
+  console.log(`🚀 Running on ${process.env.PORT}`)
+);
